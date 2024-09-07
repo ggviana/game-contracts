73efa4c91851e6fe464c6ae467ee8570c3a74177
Guilherme Guimarães <ggviana@hotmail.com.br>
2024-09-07 16:23:40 -0300

    Moved GamePositions

--- src/Game.sol
+++ src/Game.sol
@@ -1,38 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-import "../lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-
-contract GamePositionToken1155 is ERC1155 {
-    address public game;
-
-    constructor() ERC1155("") {
-        game = msg.sender; // The game contract becomes the minter
-    }
-
-    // Only the Game contract can mint tokens
-    function mint(
-        address to,
-        string memory id,
-        uint256 amount
-    ) external {
-        require(msg.sender == game, "Only game can mint");
-        uint tokenId = uint(keccak256(abi.encodePacked(id))); // Generate unique ID based on string
-        _mint(to, tokenId, amount, "");
-    }
-
-    // Only the Game contract can burn tokens
-    function burn(
-        address from,
-        string memory id,
-        uint256 amount
-    ) external {
-        require(msg.sender == game, "Only game can burn");
-        uint tokenId = uint(keccak256(abi.encodePacked(id))); // Generate unique ID based on string
-        _burn(from, tokenId, amount);
-    }
-}
 
 contract Game {
     struct PlayerBet {
@@ -52,7 +21,7 @@ contract Game {
     GameStatus public status;
     mapping(address => PlayerBet) public playerBets;
     mapping(address => bool) public hasClaimedReward;
-    GamePositionToken1155 public positionTokenContract;
+    GamePositions public positionTokenContract;
     string[] public options; // List of option names (e.g., ["yes", "no"])
 
     event OptionPicked(address indexed player, string optionName, uint amount);
---
+++ src/GamePositions.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+
+contract GamePositions is ERC1155 {
+    address public game;
+
+    constructor() ERC1155("") {
+        game = msg.sender; // The game contract becomes the minter
+    }
+
+    // Only the Game contract can mint tokens
+    function mint(
+        address to,
+        string memory id,
+        uint256 amount
+    ) external {
+        require(msg.sender == game, "Only game can mint");
+        uint tokenId = uint(keccak256(abi.encodePacked(id))); // Generate unique ID based on string
+        _mint(to, tokenId, amount, "");
+    }
+
+    // Only the Game contract can burn tokens
+    function burn(
+        address from,
+        string memory id,
+        uint256 amount
+    ) external {
+        require(msg.sender == game, "Only game can burn");
+        uint tokenId = uint(keccak256(abi.encodePacked(id))); // Generate unique ID based on string
+        _burn(from, tokenId, amount);
+    }
+}
